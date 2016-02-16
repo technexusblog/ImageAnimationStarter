1cae938536e69dc8fa4d031e74c1fd0a26ccdfe0
Kavi <kavi.j@augmentetechstudio.com>
2016-02-16 15:52:36 +0530

    Added initial resources

---
+++ ImageAnimationStarter/ImageAnimationStarter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageAnimationStarter
+//
+//  Created by AUGMENTe TechSTudio Pvt Ltd on 16/02/16.
+//  Copyright © 2016 TechNexus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
